2f4ed99cfcedeb9558e202e46d5d33e20c018e1a
DangTuanGRE <dangviettuangre@gmail.com>
2025-03-14 11:39:01 +0700

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import Navbar from "../components/Navbar/Navbar";
 
 function Home() {
-    return (
-        <div>
-            <Navbar />
-            <h1>Welcome to Home Page</h1>
-            
-        </div>
-    );
-} 
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to Home Page</h1>
+      <h2>ThangThu2</h2>
+    </div>
+  );
+}
 
 export default Home;
